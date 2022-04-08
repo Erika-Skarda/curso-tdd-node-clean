c93fd6d7ba6ed1231655a65a92d42fba62e352cc
Erika-Skarda <erikaskarda@yahoo.com.br>
2022-04-08 16:19:00 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

---
+++ src/presentation/protocols/http.ts
@@ -0,0 +1,9 @@
+//protocols ficam todas as interfaces relacioandas a presetantion
+export interface HttpResponse {
+    statusCode: number
+    body: any
+}
+
+export interface HttpRequest {
+    body?: any
+}
