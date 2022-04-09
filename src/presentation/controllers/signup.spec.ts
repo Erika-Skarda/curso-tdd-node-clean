5a2926cd3c694b7ac219b7ec6a5bb592734f788a
Erika-Skarda <erikaskarda@yahoo.com.br>
2022-04-09 20:45:36 -0300

    test: ensure SignUpController returns 400 if no passwor is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -32,3 +32,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController() // sut system under test => pra identificar quem estamos testando
+    const httpRequest = {
+      body: { 
+        name: 'an_email@email.com',
+        email: 'an_email@email.com',
+        password_confirmation: 'any_password_confirmation'
+      }
+    } // função pra validar o request e retornar um res
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+})
