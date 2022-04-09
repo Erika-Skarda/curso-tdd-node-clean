34673aa6ad95e36f68669e64699773a8ab3a37b1
Erika-Skarda <erikaskarda@yahoo.com.br>
2022-04-09 20:30:24 -0300

    refactor: move a duplicated code to a helper method

--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,9 +1,9 @@
 //protocols ficam todas as interfaces relacioandas a presetantion
 export interface HttpResponse {
-    statusCode: number
-    body: any
+  statusCode: number
+  body: any
 }
 
 export interface HttpRequest {
-    body?: any
+  body?: any
 }
