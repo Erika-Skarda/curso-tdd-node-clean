9c2cc8e22bed567098027124cdb984276be6f9dd
Erika-Skarda <erikaskarda@yahoo.com.br>
2022-04-10 21:48:21 -0300

    refactor: change mocking strategy

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -20,14 +20,15 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid(email: string): boolean {
-      throw new Error
-    }
-  }
-  return new EmailValidatorStub()
-}
+// const makeEmailValidatorWithError = (): EmailValidator => {
+//   class EmailValidatorStub implements EmailValidator {
+//     isValid(email: string): boolean {
+//       throw new Error
+//     }
+//   }
+//   return new EmailValidatorStub()
+// }
+
 //factory
 //Factory Method Pattern allows us to construct a 
 //new object without calling constructor directly.
@@ -134,9 +135,11 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 500 if EmailValidator throws', () => {
-   
-    const emailValidatorStub = makeEmailValidatorWithError()
-    const sut = new SignUpController(emailValidatorStub)
+   const { sut, emailValidatorStub } = makeSut()
+
+   jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+     throw new Error
+   })
     
     const httpRequest = {
       body: { 
