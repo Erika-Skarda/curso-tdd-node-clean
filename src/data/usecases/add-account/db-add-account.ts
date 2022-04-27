28fc44072bf6713c41b11dd4e25ce00a569f160f
Erika-Skarda <erikaskarda@yahoo.com.br>
2022-04-26 22:47:33 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -2,18 +2,30 @@ import {
   AddAccount, 
   AddAccountModel, 
   AccountModel,
-  Encrypter
+  Encrypter,
+  AddAccountRepository
  } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) {
-    this.encrypter =  encrypter
+  constructor (
+    encrypter: Encrypter,
+    addAccountRepository: AddAccountRepository) {
+    this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
-  async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    // copia todo accountData e altera a opção password, 
+    //o {} garante o original vou criar obj novo e inserir tudo que tinha e no accountData + hashed password
+    await this.addAccountRepository.add(Object.assign(
+      {},
+      accountData, 
+      { password: hashedPassword }
+    ))
     return new Promise(resolve => resolve(null))
   }
 }
